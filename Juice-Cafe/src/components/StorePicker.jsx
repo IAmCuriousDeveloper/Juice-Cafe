16e61184a736073932d6ea95a4c9141b41625a54
Prashant rawal <Prashantrawal619@gmail.com>
2019-02-26 17:09:56 +0530

    added order,inventory,and header component

--- Juice-Cafe/src/components/StorePicker.jsx
+++ Juice-Cafe/src/components/StorePicker.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-class StorePicker extends React.Component{
-    render(){
-        return(
-            <p>i am the store</p>
-        )
-    }
+import React from "react";
+class StorePicker extends React.Component {
+  render() {
+    // making a fragment and doing jsx stuff ,also use ( ) in return statement coz javaScript use asi(automatic semicolon insertion)
+    return (
+      <>
+        <form action="" className="store-selector">
+          <h2>Please choose Store</h2>
+          <input type="text" required placeholder="Choose our store" />
+          <button type="submit">Visit</button>
+        </form>
+        {/* this is a comment on jsx  and it only work under some tags */}
+      </>
+    );
+  }
 }
+
+
 export default StorePicker;
