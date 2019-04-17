a58842610094ebd48d32e9f6ef1cfb9aecfbc627
Prashant rawal <Prashantrawal619@gmail.com>
2019-04-17 17:02:27 +0530

    added the authentication for facebook and github

---
+++ Juice-Cafe/src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <nav className='login'>
+        <h2>Login to your shop inventory</h2>
+        <p>sign-in to manage your shop's inventory</p>
+        <button
+          className='facebook'
+          onClick={() => this.props.authenticate("Facebook")}
+        >
+          {" "}
+          Log in with Facebook{" "}
+        </button>
+        <button
+          className='github'
+          onClick={() => this.props.authenticate("Github")}
+        >
+          {" "}
+          Log in with Github{" "}
+        </button>
+      </nav>
+    );
+  }
+}
+
+export default Login;
