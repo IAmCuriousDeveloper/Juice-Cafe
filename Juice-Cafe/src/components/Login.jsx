aca36721d6ad1f1c3bd9e1a1d8764f8ddaab4e19
Prashant rawal <Prashantrawal619@gmail.com>
2020-01-16 17:26:17 +0530

    removed redundant code

--- Juice-Cafe/src/components/Login.jsx
+++ Juice-Cafe/src/components/Login.jsx
@@ -3,23 +3,24 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-      <nav className='login'>
+      <nav className="login">
         <h2>Login to your shop inventory</h2>
         <p>sign-in to manage your shop's inventory</p>
         <button
-          className='facebook'
+          className="facebook"
           onClick={() => this.props.authenticate("Facebook")}
         >
           {" "}
           Log in with Facebook{" "}
         </button>
         <button
-          className='github'
+          className="github"
           onClick={() => this.props.authenticate("Github")}
         >
           {" "}
           Log in with Github{" "}
         </button>
+        <p>authentication is under-construction</p>
       </nav>
     );
   }
